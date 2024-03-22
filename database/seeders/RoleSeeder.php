e52b5a290b2e66efdca7a41ab5eca2e9f5876a77
Syahrul Safarila <57931052+SySafarila@users.noreply.github.com>
2024-03-22 09:58:37 +0700

    update roles seeder

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -17,6 +17,11 @@ class RoleSeeder extends Seeder
         $superAdmin = Role::create(['name' => 'super admin']);
         $admin = Role::create(['name' => 'admin']);
         $user = Role::create(['name' => 'user']);
+        $dosen = Role::create(['name' => 'dosen']);
+
+        $tendik = Role::create(['name' => 'tendik']); // tendik: tenaga pendidik contohnya asisten lab
+        $staff = Role::create(['name' => 'staff']);
+        $mahasiswa = Role::create(['name' => 'mahasiswa']);
 
         $superAdmin->syncPermissions(['admin-access']);
         $admin->syncPermissions(['admin-access']);
