05841a0d2f0ffd572a53795e0fc917a02b2b8464
Syahrul Safarila <57931052+SySafarila@users.noreply.github.com>
2024-03-22 12:45:33 +0700

    prepare users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,9 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->text('image')->nullable();
-            $table->string('email')->unique();
+            $table->string('email')->nullable()->unique();
+            $table->string('identifier');
+            $table->string('identifier_number')->nullable()->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
