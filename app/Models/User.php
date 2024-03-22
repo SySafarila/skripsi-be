794b5ed384488b68b86f6a414912398af19a0847
Syahrul Safarila <57931052+SySafarila@users.noreply.github.com>
2024-03-22 13:44:42 +0700

    prepare user presence quotas table

--- app/Models/User.php
+++ app/Models/User.php
@@ -83,4 +83,8 @@ class User extends Authenticatable implements MustVerifyEmail
     {
         return $this->hasMany(UserFeedback::class, 'sender_id', 'id');
     }
+
+    public function presenceQuota() {
+        return $this->hasOne(UserPresenceQuota::class);
+    }
 }
