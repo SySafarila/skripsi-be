1d092bfadf0541efadefdc269b20e3d6c2da7f69
Syahrul Safarila <57931052+SySafarila@users.noreply.github.com>
2024-03-22 13:26:52 +0700

    prepare user feedback table

---
+++ app/Models/UserFeedback.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserFeedback extends Model
+{
+    use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function sender()
+    {
+        return $this->belongsTo(User::class, 'sender_id', 'id');
+    }
+
+    public function kpi()
+    {
+        return $this->belongsTo(KpiPeriod::class);
+    }
+
+    public function feedback_question()
+    {
+        return $this->belongsTo(FeedbackQuestion::class);
+    }
+}
---
+++ database/migrations/2024_03_22_060906_create_user_feedback_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Models\FeedbackQuestion;
+use App\Models\KpiPeriod;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_feedback', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class)->constrained();
+            $table->foreignIdFor(User::class, 'sender_id')->constrained('users', 'id');
+            $table->foreignIdFor(KpiPeriod::class)->constrained();
+            $table->foreignIdFor(FeedbackQuestion::class)->constrained();
+            $table->text('question');
+            $table->integer('point');
+            $table->text('message');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_feedback');
+    }
+};
