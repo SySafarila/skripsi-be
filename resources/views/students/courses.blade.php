9394197e0acac3f856eb88fa384dc2a5b157f098
Syahrul Safarila <57931052+SySafarila@users.noreply.github.com>
2024-06-13 10:21:36 +0700

    update feedback views for students & build assets

--- resources/views/students/courses.blade.php
+++ resources/views/students/courses.blade.php
@@ -1,61 +1,57 @@
 <x-app-layout>
     <div class="flex flex-col gap-4">
-        <h1 class="text-2xl font-bold">Feedback</h1>
         <x-app.session-notifications />
-        <table class="w-full border-collapse border">
-            <tr>
-                <th class="border p-2">#</th>
-                <th class="border p-2 text-left">Mata Kuliah</th>
-                <th class="border p-2 text-left">Dosen</th>
-                <th class="border p-2">Feedback</th>
-            </tr>
-            @foreach ($courses as $course)
-                <tr>
-                    <td class="border p-2 text-center">{{ $n++ }}</td>
-                    <td class="border p-2">{{ $course->name }}</td>
-                    <td class="border p-2">{{ $course->user->name ?? '-' }}</td>
-                    <td class="border p-2 text-center">
-                        <a href="{{ route('student.courses.feedback', ['course_id' => $course->id]) }}"
-                            class="btn whitespace-nowrap bg-blue-500 text-white hover:bg-blue-600">Masukan
-                            {{ $sent_feedbacks->where('course_id', $course->id)->count() }}/{{ $eduQuestions->count() }}</a>
-                    </td>
-                </tr>
-            @endforeach
-            @empty($courses->count())
-                <tr>
-                    <td class="border p-2 text-center" colspan="4">Kosong</td>
-                </tr>
-            @endempty
-        </table>
-        <table class="w-full border-collapse border">
-            <tr>
-                <th class="border p-2">#</th>
-                <th class="border p-2 text-left">Bagian</th>
-                <th class="border p-2">Feedback</th>
-            </tr>
-            @foreach ($nonEduQuestions as $tendik_position_id)
-                @foreach ($tendik_position_id as $key => $nonEduQuestion)
-                    @if ($key == 0)
-                        <tr>
-                            <td class="border p-2 text-center">{{ $nn++ }}</td>
-                            <td class="border p-2">{{ $nonEduQuestion->to->division }}</td>
-                            <td class="border p-2 text-center">
-                                <a href="{{ route('student.courses.feedback.nonedu', ['tendik_position_id' => $nonEduQuestion->to->id]) }}"
-                                    class="btn whitespace-nowrap bg-blue-500 text-white hover:bg-blue-600">Masukan
-                                    {{ $sent_feedbacks->where('tendik_position_id', $nonEduQuestion->to->id)->count() }}/{{ $tendik_position_id->where('tendik_position_id', $nonEduQuestion->to->id)->count() }}</a>
-                            </td>
-                        </tr>
-                    @endif
+        <h1 class="text-2xl font-bold">Feedback</h1>
+        <div class="-mt-2 flex flex-col gap-2">
+            <h2 class="text-xl font-bold">Dosen / Mata Kuliah</h2>
+            <div class="divide-y">
+                @foreach ($courses as $course)
+                    <a href="{{ route('student.courses.feedback', ['course_id' => $course->id]) }}" class="flex flex-col p-2 rounded-md hover:bg-gray-100">
+                        <div class="flex items-center gap-2">
+                            <img src="{{ $course->user->image ? asset('storage/' . $course->user->image) : asset('images/profile.png') }}" class="w-10 h-10 rounded-full" alt="Photo profile">
+                            <div class="flex flex-col w-full">
+                                <span class="font-semibold line-clamp-1 break-all">{{ $course->user->name }}</span>
+                                <span class="-mt-1 flex justify-between items-center">
+                                    <span class="w-full line-clamp-1 break-all">{{ $course->name }}</span>
+                                    <small class="shrink-0 line-clamp-1 break-all"><b>{{ $eduQuestions->count() }}/{{ $sent_feedbacks->where('course_id', $course->id)->count() }}</b> terkirim</small>
+                                </span>
+                            </div>
+                        </div>
+                    </a>
                 @endforeach
-            @endforeach
-        </table>
+            </div>
+        </div>
+        <div class="flex flex-col gap-2">
+            <h2 class="text-xl font-bold">TenDik / Divisi</h2>
+            <div class="divide-y">
+                @foreach ($nonEduQuestions as $tendik_position_id)
+                    @foreach ($tendik_position_id as $key => $nonEduQuestion)
+                        @if ($key == 0)
+                            <a href="{{ route('student.courses.feedback.nonedu', ['tendik_position_id' => $nonEduQuestion->to->id]) }}" class="flex flex-col p-2 rounded-md hover:bg-gray-100">
+                                <div class="flex items-center gap-2">
+                                    <img src="{{ asset('icons/division.svg') }}" class="w-10 h-10 bg-black rounded-full p-1.5" alt="Photo profile">
+                                    <div class="flex flex-row justify-between items-center w-full">
+                                        <span class="font-semibold line-clamp-1 break-all">{{ $nonEduQuestion->to->division }}</span>
+                                        <small class="line-clamp-1 shrink-0 break-all"><b>{{ $tendik_position_id->where('tendik_position_id', $nonEduQuestion->to->id)->count() }}/{{ $sent_feedbacks->where('tendik_position_id', $nonEduQuestion->to->id)->count() }}</b> terkirim</small>
+                                    </div>
+                                </div>
+                            </a>
+                        @endif
+                    @endforeach
+                @endforeach
+            </div>
+        </div>
         @if ($valid_kpi !== true)
             <div>
-                <p class="text-center">Tanggal KPI ({{ \Carbon\Carbon::parse($active_kpi->start_date)->format('d/m/Y') }} - {{ \Carbon\Carbon::parse($active_kpi->end_date)->format('d/m/Y') }})</p>
+                <p class="text-center">Tanggal KPI
+                    ({{ \Carbon\Carbon::parse($active_kpi->start_date)->format('d/m/Y') }} -
+                    {{ \Carbon\Carbon::parse($active_kpi->end_date)->format('d/m/Y') }})</p>
             </div>
         @else
             <div>
-                <p class="text-center">Tanggal KPI ({{ \Carbon\Carbon::parse($active_kpi->start_date)->format('d/m/Y') }} - {{ \Carbon\Carbon::parse($active_kpi->end_date)->format('d/m/Y') }})</p>
+                <p class="text-center">Tanggal KPI
+                    ({{ \Carbon\Carbon::parse($active_kpi->start_date)->format('d/m/Y') }} -
+                    {{ \Carbon\Carbon::parse($active_kpi->end_date)->format('d/m/Y') }})</p>
             </div>
         @endif
     </div>
