4408f48665cf20b40f3e07928f836c2daef2da72
Syahrul Safarila <57931052+SySafarila@users.noreply.github.com>
2024-06-25 08:21:46 +0700

    v0.0.26-beta

--- config/app.php
+++ config/app.php
@@ -215,5 +215,5 @@ return [
         'Excel' => Maatwebsite\Excel\Facades\Excel::class,
     ])->toArray(),
 
-    'version' => env('APP_VERSION', 'v0.0.25-beta')
+    'version' => env('APP_VERSION', 'v0.0.26-beta')
 ];
